928a605bef3ba00be9924975e308d48c53d55944
nouser <no@email.no>
2024-03-07 15:20:05 +0100

    start of rna seq

---
+++ 5-2/variantCallingBcfTools.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+outfolder="/mnt/silo/hts2024/alehmann/hts-exercises/5-2/mappingGem"
+mkdir "$outfolder"
+crubellaData="/mnt/silo/hts2024/alehmann/hts-exercises/5-1/crubellaData/ncbi_dataset/data"
+gemFolder="/mnt/silo/hts2024/alehmann/libs/gem/gem3-mapper/bin/"
+
+"$gemFolder/gem-indexer" -i "$crubellaData/idx/ref" -o "$crubellaData/idx/refGem"
+
+dir="/mnt/silo/hts2024/ckappel/exercises/ex04-01-02-subset-preprocessing/subsets-phred33-bfc"
+dest=$(basename "$dir")
+outfile="$outfolder/$dest"
+"$gemFolder/gem-mapper" -I "$crubellaData/idx/refGem.gem" -1 "$dir"/Cgr_1.fq.gz -2 "$dir"/Cgr_2.fq.gz -o "$outfile".sam
+samtools view -hSbo "$outfile".bam "$outfile".sam
+samtools sort "$outfile".bam -o "$outfile".sorted.bam
+samtools index "$outfile".sorted.bam
+bcftools2 mpileup -f "$crubellaData/idx/ref" "$outfile".sorted.bam | bcftools2 call -mv -Ob > "$outfile".bcf
+bcftools2 view "$outfile".bcf > "$outfile".vcf
--- 5-2/variantCallingFreeBayes.sh
+++ 5-2/variantCallingFreeBayes.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-outfolder="/mnt/silo/hts2024/alehmann/hts-exercises/5-2/mappingGem"
+outfolder="/mnt/silo/hts2024/alehmann/hts-exercises/5-2/variantCallingFreebayes"
 mkdir "$outfolder"
 crubellaData="/mnt/silo/hts2024/alehmann/hts-exercises/5-1/crubellaData/ncbi_dataset/data"
 gemFolder="/mnt/silo/hts2024/alehmann/libs/gem/gem3-mapper/bin/"
 
 "$gemFolder/gem-indexer" -i "$crubellaData/idx/ref" -o "$crubellaData/idx/refGem"
 
-dir="/mnt/silo/hts2024/data/Capsella-sequencing/DNA-seq/subsets-phred33/"
+dir="/mnt/silo/hts2024/ckappel/exercises/ex04-01-02-subset-preprocessing/subsets-phred33-bfc"
 dest=$(basename "$dir")
 outfile="$outfolder/$dest"
-"$gemFolder/gem-mapper" -I "$crubellaData/idx/refGem.gem" -1 "$dir"/Cgr_1.fq.gz -2 "$dir"/Cgr_2.fq.gz -o "$outfile".sam
+"$gemFolder/gem-mapper" -I "$crubellaData/idx/refGem.gem" -1 "$dir"/Cru_1.fq.gz -2 "$dir"/Cru_2.fq.gz -o "$outfile".sam
 samtools view -hSbo "$outfile".bam "$outfile".sam
 samtools sort "$outfile".bam -o "$outfile".sorted.bam
 samtools index "$outfile".sorted.bam
-bcftools2 mpileup -f "$outfile".sorted.bam | bcftools2 call -mv -Ob > "$outfile".bcf
-bcftools2 view "$outfile".bcf > "$outfile".vcf
+freebayes -f "$crubellaData/idx/ref" "$outfile".sorted.bam > "$outfile.vcf"
